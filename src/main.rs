3ae194966a10e30132aa2994e35143bdcd6daf1f
lxhcaicai <2778763221@qq.com>
2023-07-30 23:59:55 +0800

    当前服务器实现中模拟慢请求

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::fs;
+use std::{fs, thread};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -17,8 +18,12 @@ fn handle_connection(mut stream:TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
