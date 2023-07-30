7e5ae5d713831359f61bc02be30503c8f90de968
lxhcaicai <2778763221@qq.com>
2023-07-31 00:33:24 +0800

    异步执行连接的线程池 改造多线程服务器

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::{fs, thread};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread::Thread;
 use std::time::Duration;
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
